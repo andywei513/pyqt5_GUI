cf0492817ab4aff3229aea7a6e099b4767d05a68
andy513 <cfa2andy@163.com>
2018-04-05 23:29:49 +0800

    Initial commit

---
+++ GUI 1.py
@@ -0,0 +1,10 @@
+# _*_ coding:utf-8 _*_  
+# @Author:Andywei time:2018/4/5 
+
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+app = QApplication(sys.argv)
+MainWindow = QMainWindow()
+MainWindow.show()
+sys.exit(app.exec_())
